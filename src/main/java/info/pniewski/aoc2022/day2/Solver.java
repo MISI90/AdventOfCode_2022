b607cad7b1a445881549473e3046721b78bb87b0
MISI90 <cydziol@gmail.com>
2022-12-03 16:00:22 +0100

    Day 2

---
+++ src/main/java/info/pniewski/aoc2022/day2/Solver.java
@@ -0,0 +1,101 @@
+package info.pniewski.aoc2022.day2;
+
+import info.pniewski.aoc2022.helper.PuzzleSolver;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.BiFunction;
+
+class Solver implements PuzzleSolver {
+
+    @Override
+    public String solvePuzzle(List<String> inputs) {
+        return solve(inputs, (option, s) -> Option.getOption(s));
+    }
+
+    @Override
+    public String solvePuzzle2(List<String> inputs) {
+        return solve(inputs, this::getOptionForExpectedResult);
+    }
+
+    /**
+     * Solve the puzzle.
+     *
+     * @param inputs                    input line
+     * @param calculateOptionForPlayer2 {@link BiFunction} for calculating the correct option for player 2, inputs are {@link Option} - player 1 selected option, {@link String} - player 2 expected result in form of X, Y, Z
+     * @return puzzle answer
+     */
+    private String solve(List<String> inputs, BiFunction<Option, String, Option> calculateOptionForPlayer2) {
+        int sum = 0;
+        for (String input : inputs) {
+            var battle = input.split(" ");
+            var player1 = Option.getOption(battle[0]);
+            var player2 = calculateOptionForPlayer2.apply(player1, battle[1]);
+
+            sum = calculateAndAddResult(sum, player1, player2);
+        }
+        return String.valueOf(sum);
+    }
+
+    private Option getOptionForExpectedResult(Option player1, String expectedResult) {
+        if (expectedResult.equals("Y")) {
+            return player1;
+        } else if (expectedResult.equals("X")) {
+            if (Objects.requireNonNull(player1) == Option.ROCK) {
+                return Option.SCISSOR;
+            } else if (player1 == Option.PAPER) {
+                return Option.ROCK;
+            }
+            return Option.PAPER;
+        }
+        if (Objects.requireNonNull(player1) == Option.ROCK) {
+            return Option.PAPER;
+        } else if (player1 == Option.PAPER) {
+            return Option.SCISSOR;
+        }
+        return Option.ROCK;
+    }
+
+    private int calculateAndAddResult(int sum, Option player1, Option player2) {
+        if (player1 == player2) {
+            sum = sum + 3;
+        } else if (player2Wins(player1, player2)) {
+            sum = sum + 6;
+        }
+        sum = sum + player2.getScore();
+        return sum;
+    }
+
+    private boolean player2Wins(Option player1, Option player2) {
+        return player2 == Option.ROCK && player1 == Option.SCISSOR || player2 == Option.PAPER && player1 == Option.ROCK || player2 == Option.SCISSOR && player1 == Option.PAPER;
+    }
+
+    private enum Option {
+        ROCK(1, "A", "X"),
+        PAPER(2, "B", "Y"),
+        SCISSOR(3, "C", "Z");
+
+        private final int score;
+        private final String[] chars;
+
+        Option(int score, String... chars) {
+            this.score = score;
+            this.chars = chars;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public static Option getOption(String value) {
+            for (Option option : Option.values()) {
+                if (Arrays.asList(option.chars).contains(value)) {
+                    return option;
+                }
+            }
+            return null;
+        }
+    }
+
+}
---
+++ src/test/java/info/pniewski/aoc2022/day2/SolverTest.java
@@ -0,0 +1,31 @@
+package info.pniewski.aoc2022.day2;
+
+import info.pniewski.aoc2022.helper.PuzzleSolver;
+import info.pniewski.aoc2022.helper.PuzzleSolverTest;
+
+public class SolverTest extends PuzzleSolverTest {
+    @Override
+    protected PuzzleSolver getSolver() {
+        return new Solver();
+    }
+
+    @Override
+    protected String getDay() {
+        return "2";
+    }
+
+    @Override
+    protected String getExpectedCorrectResult() {
+        return "15";
+    }
+
+    @Override
+    protected String getExpectedPuzzle1Result() {
+        return "9241";
+    }
+
+    @Override
+    protected String getExpectedPuzzle2Result() {
+        return "14610";
+    }
+}

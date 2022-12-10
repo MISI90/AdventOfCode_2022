8e652651c18aa016948fab6d713c55cf962e606a
MISI90 <cydziol@gmail.com>
2022-12-10 16:31:39 +0100

    Add utils classes

--- src/main/java/info/pniewski/aoc2022/day8/Solver.java
+++ src/main/java/info/pniewski/aoc2022/day8/Solver.java
@@ -1,6 +1,7 @@
 package info.pniewski.aoc2022.day8;
 
 import info.pniewski.aoc2022.helper.PuzzleSolver;
+import info.pniewski.aoc2022.utils.Position;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -97,36 +98,4 @@ class Solver implements PuzzleSolver {
         return grid;
     }
 
-    private static class Position {
-        public final int row;
-        public final int column;
-
-        public Position(int row, int column) {
-            this.row = row;
-            this.column = column;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Position position = (Position) o;
-
-            if (row != position.row) return false;
-            return column == position.column;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = row;
-            result = 31 * result + column;
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("Position{row=%d, column=%d}", row, column);
-        }
-    }
 }
